3213e3a0de86575fde6357d687ffd1316af1bc2b
sara-jr <salazar.ramirez.j@hotmail.com>
2022-10-06 11:35:33 -0500

    Creada la pagina para ver los datos del perfil de usuario

--- controlador/controlador.php
+++ controlador/controlador.php
@@ -1,3 +1,9 @@
 <?php
-
+function alert($msg){
+    echo "
+    <script>
+        alert($msg);
+    </script>
+    ";
+}
 ?>
--- modelo/modelo.php
+++ modelo/modelo.php
@@ -4,67 +4,78 @@ class Modelo{
 	function __construct($con){
 		$this->conn=$con;
 	}
-function validarusuario($params){
-	$error = "";
-	$valor = "";
-	$user = $params ["usuario"];
-	$pass = $params ["pass"];
+	
+	function validarusuario($params){
+		$error = "";
+		$valor = "";
+		$user = $params ["usuario"];
+		$pass = $params ["pass"];
 
-	$query = "SELECT * FROM usuarios WHERE usuario = '".$user."' AND password = '".$pass."'; ";
-	$resultado = mysqli_query($this->conn, $query);
+		$query = "SELECT * FROM usuarios WHERE usuario = '".$user."' AND password = '".$pass."'; ";
+		$resultado = mysqli_query($this->conn, $query);
 
-	if(mysqli_num_rows($resultado)!= 0){
-		$valor = "OK";
-		session_start();
-		$_SESSION["logueado"] = TRUE;
-		
-		while($row = mysqli_fetch_array($resultado))
-		{
-			$_SESSION["nombre"] = $row['nombre'];
-			$_SESSION["usuario"] = $row['usuario'];
-			$_SESSION["tipo"] = $row['tipo'];
-		}
+		if(mysqli_num_rows($resultado)!= 0){
+			$valor = "OK";
+			session_start();
+			$_SESSION["logueado"] = TRUE;
+			
+			while($row = mysqli_fetch_array($resultado)){
+				$_SESSION["id"] = $row['id'];
+				$_SESSION["nombre"] = $row['nombre'];
+				$_SESSION["usuario"] = $row['usuario'];
+				$_SESSION["tipo"] = $row['tipo'];
+			}
 		}
 		$resul[] = $valor;
 		$resul[] = $error;
 		return $resul;
-		}
+	}
 		
 
-function agregaUsuario( $params ){
+	function agregaUsuario( $params ){
 
-			$error = "";
-			$valor = "";
-			$nombre = $params["nombre"];
-			$ape1 = $params["ape1"];
-			$telefono = $params["telefono"];
-			$email = $params["email"];
-			$edad = $params["edad"];
-			$usuario = $params["usuario"];
-			$pass = $params["pass"];
-            $tipo = $params["tipo"];
-			$sqlValidar = "SELECT * FROM usuarios WHERE usuario = '".$usuario."' OR correo = '".$email."'  ";
-			$resultado = mysqli_query($this->conn, $sqlValidar);
+		$error = "";
+		$valor = "";
+		$nombre = $params["nombre"];
+		$ape1 = $params["ape1"];
+		$telefono = $params["telefono"];
+		$email = $params["email"];
+		$edad = $params["edad"];
+		$usuario = $params["usuario"];
+		$pass = $params["pass"];
+		$tipo = $params["tipo"];
+		$sqlValidar = "SELECT * FROM usuarios WHERE usuario = '".$usuario."' OR correo = '".$email."'  ";
+		$resultado = mysqli_query($this->conn, $sqlValidar);
+		
+		
+		if(mysqli_num_rows($resultado)!= 0){				
+			$error="d";
+		}else{
 			
-			
-			if(mysqli_num_rows($resultado)!= 0){				
-				$error="d";
+			$query = "INSERT INTO usuarios(nombre, apellido, telefono, correo, edad, usuario, password, tipo)";
+			$query .= " VALUES ('".$nombre."', '".$ape1."', '".$telefono."', '".$email."', '".$edad."', '".$usuario."', '".$pass."', 0);";
+
+			if($this->conn->query($query)){	
+				$valor = $this->conn->affected_rows;		
 			}else{
-				
-				$query = "INSERT INTO usuarios(nombre, apellido, telefono, correo, edad, usuario, password, tipo)";
-				$query .= " VALUES ('".$nombre."', '".$ape1."', '".$telefono."', '".$email."', '".$edad."', '".$usuario."', '".$pass."', 0);";
-
-				if($this->conn->query($query)){	
-					$valor = $this->conn->affected_rows;		
-				}else{
-					$error = '[' . $this->conn->error . ']';
-				}
-									
-			}	 
-			 
-			$resul[] = $valor;
-			$resul[] = $error;	
-			return $resul;
-		}
+				$error = '[' . $this->conn->error . ']';
 			}
-		?>
+								
+		}	 
+			 
+		$resul[] = $valor;
+		$resul[] = $error;	
+		return $resul;
+	}
+
+	function perfil( $id ){
+		$query = "SELECT * FROM usuarios WHERE id = ".$id;
+		$resultado = mysqli_query($this->conn, $query);
+		if(!$resultado){
+			$error = 'MySQL Error: '.mysqli_connect_error();
+
+		}
+		return $resultado;
+	} 
+}
+?>
