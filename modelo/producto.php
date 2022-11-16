4fde9f78dffc564c4c3c39b47fd96f560380882b
sara-jr <salazar.ramirez.j@hotmail.com>
2022-11-16 09:05:49 -0600

    Creados los modelos para las tablas de la base de datos

--- modelo/producto.php
+++ modelo/producto.php
@@ -8,4 +8,56 @@ class ModeloProducto extends BaseModel{
 	}
 }
 
+class ModeloUbicacion extends BaseModel{
+	public function __construct(){
+		parent::__construct('estado',
+			array('codigoPostal', 'calle', 'numeroExterior', 'numeroInterior'), 
+			'domicilio');
+	}
+}
+
+class ModeloUsuario extends BaseModel{
+	public function __construct(){
+		parent::__construct('usuario',
+			array( 
+				'nombre',
+				'apellido',
+				'telefono',
+				'correo',
+				'fechaDeNacimiento',
+				'usuario',
+				'password',
+				'tipo',
+				'saldo'),
+			'idUsuario');
+	}
+
+	public function unique($username, $email){
+		$result = $this->conn->query(
+			'SELECT correo, usuario FROM {$this->table_name} WHERE correo={$email} OR usuario={$username};');
+		return $result->num_rows == 0;
+	}
+	
+	public function valid_credentials($username, $pwhash){
+		$result = $this->conn->query(
+			'SELECT password, usuario FROM {$this->table_name} WHERE usuario={$username} AND pasword={$pwhash};');
+		return $result->num_rows == 1;
+	}
+}
+
+class ModeloOrden extends BaseModel{
+	public function __construct(){
+		parent::__construct(
+			'orden', array('idUsuario', 'domicilio', 'fecha'),
+			'idOrden');
+	}
+}
+
+class ModeloDevolucion extends BaseModel{
+	public function __construct(){
+		parent::__construct(
+			'devolucion', array('idOrden', 'motivo'),
+			'idDevolucion');
+	}
+}
 ?>
